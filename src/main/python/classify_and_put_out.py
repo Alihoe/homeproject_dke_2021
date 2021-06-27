e8c20f006520e685614fc5706af4c368da29642f
alihoe <alica.hoevelmeyer@live.de>
2021-06-27 16:44:16 +0200

    generate description ouput and change file format of output(no blank lines)

--- src/main/python/classify_and_put_out.py
+++ src/main/python/classify_and_put_out.py
@@ -6,13 +6,13 @@ from sklearn.preprocessing import MinMaxScaler
 
 
 def classify_and_put_out(dataset_path, classifier, input_file_name_test_plds, output_left, output_right):
-    dataset = pd.read_csv(dataset_path)
+    dataset = pd.read_csv(dataset_path, encoding = 'latin1')
     x = dataset.iloc[:, 1:].values
     scaler = MinMaxScaler()
     x = scaler.fit_transform(x)
     y_pred = classifier.predict(x)
     y_proba = classifier.predict_proba(x)
-    left = open(output_left, 'w')
+    left = open(output_left, 'a', newline='')
     csv_writer_left = csv.writer(left)
     with open(input_file_name_test_plds, newline='') as train:
         plds = csv.reader(train, delimiter=' ', quotechar='|')
@@ -22,7 +22,7 @@ def classify_and_put_out(dataset_path, classifier, input_file_name_test_plds, ou
                 csv_writer_left.writerow([pld][0] + [y_proba[count][0]])
             count += 1
     left.close()
-    right = open(output_right, 'w')
+    right = open(output_right, 'a', newline='')
     csv_writer_right = csv.writer(right)
     with open(input_file_name_test_plds, newline='') as train:
         plds = csv.reader(train, delimiter=' ', quotechar='|')
--- src/main/python/train_classifier.py
+++ src/main/python/train_classifier.py
@@ -4,7 +4,7 @@ from sklearn.preprocessing import MinMaxScaler
 
 
 def train_classifier(dataset_path):
-    dataset = pd.read_csv(dataset_path)
+    dataset = pd.read_csv(dataset_path, encoding='latin1')
     x = dataset.iloc[:, 2:].values
     y = dataset.iloc[:, 1].values
     scaler = MinMaxScaler()
