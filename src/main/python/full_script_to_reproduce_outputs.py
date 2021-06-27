4d35b58ebc83ab0033b15e27311950b44cb617c8
alihoe <alica.hoevelmeyer@live.de>
2021-06-27 11:55:18 +0200

    generate feature set out of descriptions

--- src/main/python/full_script_to_reproduce_outputs.py
+++ src/main/python/full_script_to_reproduce_outputs.py
@@ -1,4 +1,6 @@
 from hydrate_tweets import collect_hydrated_tweets_descriptions, move_hydrated_tweets_to_pol_directory_descriptions
+from generate_feature_set import generate_feature_set, generate_feature_set_descriptions
+from generate_dataset_with_features import generate_labeled_dataset_with_features
 
 input_file_name_left = '../../../input_data/left_train.csv'
 input_file_name_right = '../../../input_data/right_train.csv'
@@ -6,6 +8,7 @@ output_directory_tweet_ids = '../resources/tweet_ids/'
 output_directory_hydrated_tweets = '../resources/hydrated_tweets_texts/'
 output_directory_hydrated_tweets_descriptions = '../resources/hydrated_tweets_descriptions/'
 output_file_name_labeled_dataset = 'dataset_labeled'
+output_file_name_labeled_dataset_descriptions = 'dataset_labeled_descriptions'
 
 input_file_name_test_plds = '../../../input_data/test.csv'
 output_directory_tweet_ids_test_data = '../resources/tweet_ids_test_data/'
@@ -19,9 +22,11 @@ output_right = '../../../output_data/right.csv'
 #collect_hydrated_tweets(input_file_name_left, input_file_name_right, output_directory_hydrated_tweets, output_directory_tweet_ids)
 #collect_hydrated_tweets_descriptions(input_file_name_left, input_file_name_right, output_directory_hydrated_tweets_descriptions, output_directory_tweet_ids)
 #move_hydrated_tweets_to_pol_directory()
-move_hydrated_tweets_to_pol_directory_descriptions()
+#move_hydrated_tweets_to_pol_directory_descriptions()
 #generate_feature_set()
+#generate_feature_set_descriptions()
 #generate_labeled_dataset_with_features(output_file_name_labeled_dataset, input_file_name_left, input_file_name_right, output_directory_hydrated_tweets)
+generate_labeled_dataset_with_features(output_file_name_labeled_dataset_descriptions, input_file_name_left, input_file_name_right, output_directory_hydrated_tweets)
 #classifier = train_classifier(output_file_name_labeled_dataset)
 #prepare dataset and classify test data
 #collect_tweet_ids_per_pld(input_file_name_test_plds, output_directory_tweet_ids_test_data)
--- src/main/python/generate_feature_set.py
+++ src/main/python/generate_feature_set.py
@@ -52,3 +52,30 @@ def generate_feature_set():
         word_features_file.write('\n')
     word_features_file.close()
 
+
+def generate_feature_set_descriptions():
+    all_words = []
+    for f in os.listdir('../resources/hydrated_tweets_descriptions/left'):
+        with open('../resources/hydrated_tweets_descriptions/left/' + f, 'r', errors='ignore') as file_content:
+            text = file_content.read()
+        word_tokens = word_tokenize(text)
+        filtered = [w for w in word_tokens if w not in stop_words]
+        words = [w.lower() for w in filtered]
+        all_words.append(words)
+        all_words = flatten_lists(all_words)
+    for f in os.listdir('../resources/hydrated_tweets_descriptions/right'):
+        with open('../resources/hydrated_tweets_descriptions/right/' + f, 'r', errors='ignore') as file_content:
+            text = file_content.read()
+        word_tokens = word_tokenize(text)
+        filtered = [w for w in word_tokens if w not in stop_words]
+        words = [w.lower() for w in filtered]
+        all_words.extend(words)
+    all_words = list_sorted_by_frequency(all_words)
+    word_features = all_words[:6000]
+    print(word_features)
+    word_features_file = open('word_feature_set_descriptions.txt', 'w')
+    for element in word_features:
+        word_features_file.write(element)
+        word_features_file.write('\n')
+    word_features_file.close()
+
